e5eeb65edde69508a9418fbc6f1920e7e495b2bc
Alexsandro Percy <alexsandro.percy@gmail.com>
2022-05-30 10:31:38 -0300

    now it does not affect lava on fiery biome

--- init.lua
+++ init.lua
@@ -1,19 +1,31 @@
 local lava_nodes = {"default:lava_source","default:lava_flowing"}
 
+local function get_biome_name(pos)
+	if not pos then return "" end
+	local data = minetest.get_biome_data(pos)
+	if not data then return "" end
+	local name = minetest.get_biome_name(data.biome)
+	return name
+end
+
 minetest.register_abm({
     nodenames = lava_nodes,
     interval = 1,
     chance = 1,
     action = function(pos, node)
         if pos.y > -50 then
-            local is_protected = minetest.is_protected
-            if is_protected then
-                local owner = nil
-                if not is_protected(pos, "") then
+            if not (get_biome_name(pos) == "fiery") then
+                --isn't fiery, so lets see one more thing
+                local is_protected = minetest.is_protected
+                if is_protected then
+                    local owner = nil
+                    if not is_protected(pos, "") then
+                        --isn't protected, so block lava
+                        minetest.env:set_node(pos, {name="default:stone"})
+                    end
+                else
                     minetest.env:set_node(pos, {name="default:stone"})
                 end
-            else
-                minetest.env:set_node(pos, {name="default:stone"})
             end
         end
     end,
